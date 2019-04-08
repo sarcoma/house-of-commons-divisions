5f3f2e2db1c14909076e61b756adc00cb6335225
sarcoma <sarcoma@live.co.uk>
2019-04-08 08:56:20 +0100

    Only add Divisions and MPs that don't already exist. Pull data from Members lists.

--- models/member_of_parliament.py
+++ models/member_of_parliament.py
@@ -1,5 +1,4 @@
-
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Date
 from sqlalchemy.orm import relationship
 
 from orm import Base
@@ -10,5 +9,16 @@ class MemberOfParliament(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String)
+    clerks_id = Column(Integer)
+    dods_id = Column(Integer)
+    member_id = Column(Integer)
+    pims_id = Column(Integer)
+    full_title = Column(String)
+    date_of_birth = Column(Date)
+    date_of_death = Column(Date)
+    gender = Column(String)
     party = Column(String)
+    start_date = Column(Date)
+    end_data = Column(Date, nullable=True)
+    constituency = Column(String)
     votes = relationship("Vote", back_populates='member_of_parliament')
--- pull_members_of_parilament.py
+++ pull_members_of_parilament.py
@@ -1,3 +1,34 @@
+import requests
+from bs4 import BeautifulSoup as bs
+
+
 # Todo: Pull data about MPs from http://data.parliament.uk/membersdataplatform/memberquery.aspx#membershipinfotable
 # Note: eg http://data.parliament.uk/membersdataplatform/services/mnis/members/query/commonsmemberbetween=2012-01-01and2012-03-31/
 #       Look up data for MPs on any given division day. Compare votes against that list. Add any missing MPs to the database.
+
+def getMembersForDate(date):
+    url = 'http://data.parliament.uk/membersdataplatform/services/mnis/members/query'
+    response = requests.get('%s/commonsmemberbetween=%sand%s' % (url, date, date))
+    return bs(response.content, "lxml-xml")
+
+
+if __name__ == '__main__':
+    soup = getMembersForDate('2016-01-01')
+    members = soup.find_all('Member')
+    print(len(members))
+    for member in members:
+        print(member)
+        print(member.get('Clerks_Id'))
+        print(member.get('Dods_Id'))
+        print(member.get('Member_Id'))
+        print(member.get('Pims_Id'))
+        print(member.find('DisplayAs').string)
+        print(member.find('FullTitle').string)
+        print(member.find('Gender').string)
+        print(member.find('Party').string)
+        print(member.find('MemberFrom').string)
+        print(member.find('DateOfBirth').string)
+        print(member.find('DateOfDeath').string)
+        print(member.find('HouseStartDate').string)
+        print(member.find('HouseEndDate').string)
+        print(member.find('StartDate').string)

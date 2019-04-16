a40ccb87d3273869f6a2c9cf7892a84c60a6d7d7
sarcoma <sarcoma@live.co.uk>
2019-04-16 07:24:13 +0100

    Add json_response decorator

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from functools import wraps
+
 from flask import Flask, json
 
 from models.commons_division import CommonsDivision
@@ -8,24 +10,32 @@ app = Flask(__name__)
 
 session = session_factory()
 
+
+def json_response(func):
+    @wraps(func)
+    def wrapped_function(*args, **kwargs):
+        return app.response_class(
+            response=json.dumps(func(**kwargs), cls=AlchemyEncoder),
+            mimetype='application/json'
+        )
+
+    return wrapped_function
+
+
 @app.route('/')
+@json_response
 def commons_division_list():
     data = session.query(CommonsDivision).all()
 
-    return app.response_class(
-        response=json.dumps(data, cls=AlchemyEncoder),
-        mimetype='application/json'
-    )
+    return data
 
 
 @app.route('/<int:commons_division_id>')
+@json_response
 def commons_division_detail(commons_division_id):
     data = session.query(CommonsDivision).get(commons_division_id)
 
-    return app.response_class(
-        response=json.dumps(data, cls=AlchemyEncoder),
-        mimetype='application/json'
-    )
+    return data
 
 
 if __name__ == '__main__':

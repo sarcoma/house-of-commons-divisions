5823a5bc753c038037288c8cd263119ddc00f997
sarcoma <sarcoma@live.co.uk>
2019-05-20 22:48:42 +0100

    Working again

--- commons_divisions/create_db.py
+++
@@ -1,5 +0,0 @@
-from commons_divisions.orm.base import Base
-from commons_divisions.orm.orm import engine
-
-if __name__ == '__main__':
-    Base.metadata.create_all(engine)
--- commons_divisions/orm/orm.py
+++ commons_divisions/orm/orm.py
@@ -2,12 +2,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.pool import StaticPool
 
+from commons_divisions.models import commons_division
+from commons_divisions.models import member_of_parliament
+from commons_divisions.models import vote
+
 from commons_divisions.orm.base import Base
 
-engine = create_engine('sqlite:///commons-divisions.db',
-                       connect_args={'check_same_thread': False},
-                       poolclass=StaticPool,
-                       )
+engine = create_engine(
+    'sqlite:///./commons-divisions.db',
+    connect_args={'check_same_thread': False},
+    poolclass=StaticPool,
+)
 
 Session = sessionmaker(bind=engine)
 
